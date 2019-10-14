ffe6fa2637fbed3639c6df517111846837291304
yasmin-aumeeruddy <52958137+yasmin-aumeeruddy@users.noreply.github.com>
2019-10-14 16:26:28 +0100

    Update pre.sh

--- cloud-native-intro/pre.sh
+++ cloud-native-intro/pre.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/gcharters/open-cloud-native-intro.git;
 echo "export JAVA_HOME=/usr" >> ~/.profile;
-source ~/.profile
+source ~/.profile;
 
+git clone https://github.com/gcharters/open-cloud-native-intro.git
