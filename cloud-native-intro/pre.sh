442d1185b6e1af8cb55ff786c6cc5166554b3482
yasmin-aumeeruddy <yasmin.aumeeruddy@ibm.com>
2019-10-14 13:41:42 +0100

    nearly done yay

--- cloud-native-intro/pre.sh
+++ cloud-native-intro/pre.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "export JAVA_HOME=/usr" >> .bashrc
+echo "export JAVA_HOME=/usr" >> ~/.bashrc
 source ~/.bashrc
 git clone https://github.com/gcharters/open-cloud-native-intro.git
 docker pull open-liberty:latest
