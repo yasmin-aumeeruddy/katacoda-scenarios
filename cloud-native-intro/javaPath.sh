56d851f965cbbd80fa155ffbfde9499a81916568
yasmin-aumeeruddy <52958137+yasmin-aumeeruddy@users.noreply.github.com>
2019-10-15 10:27:21 +0100

    Update javaPath.sh

--- cloud-native-intro/javaPath.sh
+++ cloud-native-intro/javaPath.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Waiting to complete"; while [ ! -d /open-cloud-native-intro ] ; do sleep 2; done; echo "Done"
+echo "Waiting to complete"; while [ ! -d open-cloud-native-intro ] ; do sleep 2; done; echo "Done"
 source ~/.profile
